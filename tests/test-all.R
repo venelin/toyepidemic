7516f43491acdca5a0d228cab33de61ca105236d
Venelin Mitov <venelin.mitov@bsse.ethz.ch>
2017-12-11 13:57:44 +0100

    init R-project

--- tests/test-all.R
+++ tests/test-all.R
@@ -1,13 +1,13 @@
 # Copyright 2017 Venelin Mitov
 #
-# This file is part of patherit.
+# This file is part of toyepidemic.
 #
-# patherit is free software: you can redistribute it and/or modify
+# toyepidemic is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# patherit is distributed in the hope that it will be useful,
+# toyepidemic is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
